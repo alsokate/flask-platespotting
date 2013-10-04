b21d206de8cef3c7565a53cce24745e24d1fd9f4
Kate Burgett <katelyn.burgett@gmail.com>
2013-10-03 21:19:21 -0400

    Fixing zoom default from Google maps API, data passing

--- spot/static/js/script.js
+++ spot/static/js/script.js
@@ -1,39 +1,43 @@
-function EmbassyMap() {
-  var mapOptions = {
-    center: new google.maps.LatLng(38.942404, -77.066349),
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.HYBRID
-  };
+var loc
+function EmbassyMap(data) {
+  if (data[1]){ 
+    var mapOptions =
+    {
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    map1 = new google.maps.Map(document.getElementById("map1"), mapOptions);
 
-  var emb_map = new google.maps.Map(document.getElementById("map1"),
-      mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: emb_map.getCenter(),
-    map: emb_map,
-    title: 'Embassy of' + ' China'
-  });  
+    var address = data[1];
+    var geocoder1 = new google.maps.Geocoder();
+    geocoder1.geocode( { 'address': address}, function(results, status) {
+        loc = results[0].geometry.location;
+        console.log(loc);
+        if (status == google.maps.GeocoderStatus.OK) {
+          map1.setCenter(loc);
+          map1.setZoom(18);
+        }
+        var marker = new google.maps.Marker({
+          position: loc,
+          map: map1,
+          title: 'Embassy of ' + data[0]
+      });
+    });
+  }
 }
 
-var geocoder;
-var map;
-
 function CountryMap(data) {
-  console.log(this, arguments)
   var mapOptions =
   {
       mapTypeId: google.maps.MapTypeId.ROADMAP
   }
-  map = new google.maps.Map(document.getElementById("map2"), mapOptions);
+  map2 = new google.maps.Map(document.getElementById("map2"), mapOptions);
 
   var address = data[0];
   var geocoder = new google.maps.Geocoder();
   geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          map.fitBounds(results[0].geometry.bounds);
+          map2.setCenter(results[0].geometry.location);
+          map2.fitBounds(results[0].geometry.bounds);
       }
-  });
+  }); 
 }
-
-google.maps.event.addDomListener(window, 'load', EmbassyMap);
--- spot/views.py
+++ spot/views.py
@@ -27,27 +27,29 @@ def search():
 def search_results(query):
     plate_type = ''
     flag_image = ''
+    results = ''
+    location = ''
+    website = ''
     plate_type_options = {'D': 'Diplomat', 'C': 'Foreign Consul', 'S': 'Non-diplomatic Staff'}
     plate_type_index = query[0].upper()
+    country = query[1:3]
+    search = Code.query.whoosh_search(country).all()
 
     if plate_type_index in plate_type_options:
         plate_type = plate_type_options[plate_type_index]
 
-    country = query[1:3]
-    search = Code.query.whoosh_search(country).all()
-    results = search[0].country.country_name
-    location = search[0].country.mission
-    website = search[0].country.website
-
-    map_data = [results]
-
-
     if search:
+        results = search[0].country.country_name
         flag_image = results.replace(' ', '-')
+        location = search[0].country.mission
+        website = search[0].country.website
+
+    map_data = [results, location, website]
 
     return render_template('country_page.html',
         query = query,
         results = results,
+        location = location,
         plate_type = plate_type,
         flag_image = flag_image,
         data = json.dumps(map_data))
